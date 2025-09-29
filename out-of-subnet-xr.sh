ea1b133406caf225e58ab60137d60dd708bb79d6
igorderybas <146131807+igorderybas@users.noreply.github.com>
2025-09-29 13:38:19 +0200

    Update out-of-subnet-xr.sh
    Version 2 of the script:
    - Changed ip_in_subnet function for faster work
    - Filtering IPv6 EPs
    - Added info about node ID where EP is learned

--- out-of-subnet-xr.sh
+++ out-of-subnet-xr.sh
@@ -3,84 +3,73 @@
 #This script is showing out of subnets Remote EPs in the fabric.
 #To be used to evaluate impact from enabling Global Subnet Check.
 #
-#Made by Igor Derybas, in_subnet function is taken from https://unix.stackexchange.com/a/465372
+#Made by Igor Derybas
 
-function in_subnet {
-    # Determine whether IP address is in the specified subnet.
-    #
-    # Args:
-    #   sub: Subnet, in CIDR notation.
-    #   ip: IP address to check.
-    #
-    # Returns:
-    #   1|0
-    #
-    local ip ip_a mask netmask sub sub_ip rval start end
+ip_in_subnet() {
+    local ip=$1
+    local subnet=$2
 
-    # Define bitmask.
-    local readonly BITMASK=0xFFFFFFFF
+    # Split subnet into address and mask
+    local subnet_ip=${subnet%/*}
+    local mask_bits=${subnet#*/}
 
-    # Read arguments.
-    IFS=/ read sub mask <<< "${1}"
-    IFS=. read -a sub_ip <<< "${sub}"
-    IFS=. read -a ip_a <<< "${2}"
+    # Convert dotted-quad to integer
+    local ip_int=$(ip2int "$ip")
+    local subnet_int=$(ip2int "$subnet_ip")
 
-    # Calculate netmask.
-    netmask=$(($BITMASK<<$((32-$mask)) & $BITMASK))
+    # Build mask integer
+    local mask=$(( 0xFFFFFFFF << (32 - mask_bits) & 0xFFFFFFFF ))
 
-    # Determine address range.
-    start=0
-    for o in "${sub_ip[@]}"
-    do
-        start=$(($start<<8 | $o))
-    done
-
-    start=$(($start & $netmask))
-    end=$(($start | ~$netmask & $BITMASK))
-
-    # Convert IP address to 32-bit number.
-    ip=0
-    for o in "${ip_a[@]}"
-    do
-        ip=$(($ip<<8 | $o))
-    done
-
-    # Determine if IP in range.
-    (( $ip >= $start )) && (( $ip <= $end )) && rval=1 || rval=0
-
-    echo "${rval}"
+    # Compare masked values
+    if (( (ip_int & mask) == (subnet_int & mask) )); then
+        echo 1
+    else
+        echo 0
+    fi
 }
 
-#Discover Subnets deployed with VRF names
+ip2int() {
+    local IFS=.
+    read -r o1 o2 o3 o4 <<< "$1"
+    echo $(( (o1 << 24) + (o2 << 16) + (o3 << 8) + o4 ))
+}
+
+echo "Started checking Remote EPs at" `date`
+echo ""
+
+# Discover Subnets deployed with VRF names
 subnets=$(moquery -c ipv4Addr -f 'ipv4.Addr.ctrl=="pervasive"' | grep dn | sed 's/^.*dom-//' | sed 's/\/if-.*addr-\[/__/' | sed 's/\]//'| sort | uniq)
-#subnets=$(cat subnets)
 
-#Discover all XR EPs with VRF VNID
-xr=$(moquery -c epmIpEp -f 'epm.IpEp.flags=="ip"' | grep dn | sed 's/^.*\[vxlan-//' | sed 's/\].*-\[/__/' | sed 's/\]//'| sort | uniq)
-#xr=$(cat xr_u)
+# Discover all XR EPs with VRF VNID
+xr=$(moquery -c epmIpEp -f 'epm.IpEp.flags=="ip"' | grep ^dn | sed 's/.*node-//' | sed 's/\/sys\/ctx-\[vxlan-/___/' | sed 's/\].*-\[/__/' | sed 's/\]//' | grep "__" | grep -v ":" )
 
-#Discover VRF Names and VNIDs
+# Discover VRF Names and VNIDs
 vrf_vnid=$(moquery -c fvCtx  | egrep "dn|scope" | sed 's/^.*uni\/tn-/ /' | sed 's/\/ctx-/:/' | sed 's/^.*: /___/' | tr -d '\n' | tr " " "\n")
-#vrf_vnid=$(cat vrf1)
 
 for i in $xr; do
-        vnid=`echo $i | sed 's/__.*//'`
-        xr_ip=`echo $i | sed 's/^.*__//'`
+	node=`echo $i | sed 's/___.*//'`
+        vnid=`echo $i | sed 's/^.*___//' | sed 's/__.*//'`
+        xr_ip=`echo $i | sed 's/^.*__//g'`
+
         for v in $vrf_vnid; do
             xr_vrf_temp=$(echo $v | grep $vnid | sed 's/___.*//')
-            if [ "$xr_vrf_temp" != "" ]; then
+	    if [ "$xr_vrf_temp" != "" ]; then
                 xr_vrf=$xr_vrf_temp
-            fi
+	    fi
         done
         out_xr=1
         for s in $subnets; do
                 sn=$(echo $s | grep $xr_vrf | sed 's/^.*__//')
                 if [ "$sn" != "" ]; then
-                        (( $(in_subnet $sn $xr_ip) )) && echo "The $xr_ip is part of $sn in VRF $xr_vrf" && out_xr=0
-#                       (( $(in_subnet $sn $xr_ip) )) && out_xr=0\
+# Uncoment below line and comment next one if you want to see output for every good Remote EP inside a subnet.
+#                        (( $(ip_in_subnet $xr_ip $sn) )) && echo "The $xr_ip on node-$node is part of $sn in VRF $xr_vrf" && out_xr=0
+			(( $(ip_in_subnet $xr_ip $sn) )) && out_xr=0
                 fi
         done
         if [ $out_xr == 1 ]; then
-                echo "Remote EP $xr_ip is out of subnets in VRF $xr_vrf"
+                echo "Remote EP $xr_ip on node-$node is out of subnets in VRF $xr_vrf"
         fi
 done
+
+echo ""
+echo "Finished checking Remote EPs at" `date`
